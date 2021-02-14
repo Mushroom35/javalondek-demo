f9a81f484c158bc831e7ca0f8276c35c21246601
Grzybek <grzybekdawid@gmail.com>
2021-02-14 11:04:45 +0000

    [add] - sowa class created

---
+++ src/main/java/pl/sda/javalondek4/java_demo/list_vs_array/Sowa.java
@@ -0,0 +1,10 @@
+package pl.sda.javalondek4.java_demo.list_vs_array;
+
+public class Sowa extends Animal {
+
+    @Override
+    void saySomething() {
+        System.out.println("jestem sową!");
+    }
+
+}
